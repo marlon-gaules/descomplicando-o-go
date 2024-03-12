fb1dd044e1eea0bbe2e0d7687ebcb298c7878dca
marlon <marloncostaesousa@gmail.com>
2024-03-12 10:41:04 -0300

    Chamando uma API externa

---
+++ Day-7/1-chamando-api-externa/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+//da lista para baixo são strings anônimas
+type PokemonGetResponse struct {
+	Count    int64
+	Next     string
+	Previous string
+	Results  []struct {
+		Name string
+		URL  string
+	}
+}
+
+func main() {
+	url := "https://pokeapi.co/api/v2/pokemon"
+	fmt.Println("Acessando a API na url ", url)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Print(err.Error())
+		return
+	}
+	fmt.Println("O status code retornado pela url é", resp.StatusCode)
+
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Print(err.Error())
+		return
+	}
+
+	pokemonGetResponse := new(PokemonGetResponse)
+	err = json.Unmarshal(body, pokemonGetResponse)
+	if err != nil {
+		fmt.Print(err.Error())
+		return
+	}
+	fmt.Println("O nome dos 9 primeiros pokemons retornados são: ")
+	for i := 0; i < 9; i++ {
+		fmt.Println(pokemonGetResponse.Results[i].Name)
+	}
+}
